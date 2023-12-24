981d78d20cd9d2dc986438a1b5f47bffaf3e25b5
tony.origogi <tony.origogi@kakaobrain.com>
2023-12-24 22:42:46 +0900

    first commit

---
+++ Hike/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Hike
+//
+//  Created by 김정태 on 12/24/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    
+    var body: some View {
+        Image("image-1")
+            .resizable()
+            .scaledToFit()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Hike/HikeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HikeApp.swift
+//  Hike
+//
+//  Created by 김정태 on 12/24/23.
+//
+
+import SwiftUI
+
+@main
+struct HikeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
