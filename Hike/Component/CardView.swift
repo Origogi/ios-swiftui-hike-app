2496f9521cd977e79f77a1400ddf35639e3d964e
tony.origogi <tony.origogi@kakaobrain.com>
2023-12-26 00:14:24 +0900

    Update Custom buttonview

--- Hike/Component/CardView.swift
+++ Hike/Component/CardView.swift
@@ -11,18 +11,53 @@ struct CardView: View {
   var body: some View {
     ZStack {
       CustomBackgroundView()
-      ZStack {
-        Circle()
-          .fill(
-          LinearGradient(colors: [
-            Color("ColorIndigoMedium"),
-            Color("ColorSalmonLight")
-            ], startPoint: .topLeading, endPoint: .bottomTrailing)
-        ).frame(width: 256, height: 256, alignment: .center)
+      VStack {
+        //MARK : - header
 
-        Image("image-1")
-          .resizable()
-          .scaledToFit()
+        VStack(alignment: .leading) {
+          HStack {
+            Text("Hiking")
+              .fontWeight(.black)
+              .font(.system(size: 52))
+              .foregroundStyle(
+              LinearGradient(colors: [
+                  .customGrayLight,
+                  .customGrayMedium
+                ], startPoint: .top, endPoint: .bottom)
+
+            )
+            
+            Spacer()
+            
+            Button {
+              print("Button pressed")
+            } label: {
+              CustomButtonView()
+            }
+          }
+          Text("Fun and enjoyable outdoor activity for friends and families")
+            .multilineTextAlignment(.leading)
+            .italic()
+            .foregroundColor(.customGrayMedium)
+        } // : header
+        .padding(.horizontal, 30)
+
+        //MARK : - Main Content
+        ZStack {
+          Circle()
+            .fill(
+            LinearGradient(colors: [
+              Color("ColorIndigoMedium"),
+              Color("ColorSalmonLight")
+              ], startPoint: .topLeading, endPoint: .bottomTrailing)
+          ).frame(width: 256, height: 256, alignment: .center)
+
+          Image("image-1")
+            .resizable()
+            .scaledToFit()
+        }
+
+        //MARK : - footer
       }
     }.frame(width: 320, height: 570)
   }
