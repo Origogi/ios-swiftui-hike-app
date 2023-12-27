9a6b645f605ec90c78acfe55789e64aa770f3411
tony.origogi <tony.origogi@kakaobrain.com>
2023-12-28 00:03:31 +0900

    icon change

--- Hike/Screen/SettingsView.swift
+++ Hike/Screen/SettingsView.swift
@@ -8,6 +8,16 @@
 import SwiftUI
 
 struct SettingsView: View {
+
+  private let alternateAppIcons: [String] = [
+    "AppIcon-MagnifyingGlass",
+    "AppIcon-Map",
+    "AppIcon-Mushroom",
+    "AppIcon-Camera",
+    "AppIcon-Backpack",
+    "AppIcon-Campfire",
+  ]
+
   var body: some View {
     List {
       //MARK : - Section : Header
@@ -56,6 +66,38 @@ struct SettingsView: View {
       .listRowSeparator(.hidden)
 
       //MARK : - Section: Icons
+      Section(header: Text("Alternative Icons")) {
+        ScrollView(.horizontal, showsIndicators: false) {
+          HStack(spacing: 12) {
+            ForEach(alternateAppIcons.indices, id: \.self) { item in
+              Button {
+                print("Icon \(alternateAppIcons[item]) was pressed")
+                
+                UIApplication.shared.setAlternateIconName(alternateAppIcons[item]) { error in
+                  if let error = error {
+                    print("Error: \(error.localizedDescription)")
+                  } else {
+                    print("Success! You have changed the app icon.")
+                  }
+                }
+              } label: {
+                Image("\(alternateAppIcons[item])-Preview")
+                  .resizable()
+                  .scaledToFit()
+                  .frame(width: 80, height: 80)
+                  .cornerRadius(16)
+              }.buttonStyle(.borderless)
+            }
+          } //: Scrollview
+        }.padding(.top, 12)
+
+        Text("Choose your favourite app icon from the collection above")
+          .frame(minWidth: 0, maxWidth: .infinity)
+          .multilineTextAlignment(.center)
+          .foregroundColor(.secondary)
+          .font(.footnote)
+          .padding(.bottom, 12)
+      }.listRowSeparator(.hidden)
 
 
       //MARK : - Section: About
@@ -71,7 +113,7 @@ struct SettingsView: View {
         CustomListRowView(rowLabel: "Version", rowIcon: "gear", rowContent: "1.0", rowTintColor: .purple)
         CustomListRowView(rowLabel: "Developer", rowIcon: "ellipsis.curlybraces", rowContent: "Jone Doe", rowTintColor: .mint)
         CustomListRowView(rowLabel: "Designer", rowIcon: "paintpalette", rowContent: "Robert Petras", rowTintColor: .pink)
-          CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "Credo Academy", rowLinkDestination: "https://credo.academy")
+        CustomListRowView(rowLabel: "Website", rowIcon: "globe", rowTintColor: .indigo, rowLinkLabel: "Credo Academy", rowLinkDestination: "https://credo.academy")
       }
     }
   }
